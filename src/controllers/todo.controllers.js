9ae4f122739c36c5e6fc1eec383e1073e452341e
bharathk07 <97445865+bharathk07@users.noreply.github.com>
2022-03-28 10:40:00 +0530

    auth

---
+++ src/controllers/todo.controllers.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const Todo = require("../models/todo.model");
+
+//GET /todos endpoint that returns all todos of the logged in user
+router.get("", async(req,res)=>{
+    try {
+        const todos = await Todo.find().lean().exec();
+        return res.status(200).send({todos:todos});
+    } catch (error) {
+        return res.status(400).send({error:error.message});
+    }
+});
+
+//POST /todos endpoint for the logged in user to create a todo
+router.post("", async(req,res)=>{
+    try {
+        const todos = await Todo.create(req.body);
+        return res.status(200).send({todos:todos});
+    } catch (error) {
+        return res.status(400).send({error:error.message});
+    }
+});
+
+//GET /todos/:id endpoint where if the todo user is same as logged in user then
+router.get("/:id", async(req,res)=>{
+    try {
+        const todos = await Todo.findById(req.params.id).lean().exec();
+        return res.status(200).send({todos:todos});
+    } catch (error) {
+        return res.status(400).send({error:error.message});
+    }
+});
+
+//PATCH /todos/:id endpoint where if the todo user is the same as logged in user
+
+router.patch("/:id",async(req,res)=>{
+    try {
+        const todos = await Todo.findByIdAndUpdate(req.params.id, req.body,{new:true});
+        return res.status(200).send({todos:todos});
+    } catch (error) {
+        return res.status(400).send({error:error.message});
+    }
+});
+
+//delete
+router.delete("/:id",async(req,res)=>{
+    try {
+        const todos = await Todo.findByIdAndDelete(req.params.id, req.body);
+        return res.status(200).send({todos:todos});
+    } catch (error) {
+        return res.status(400).send({error:error.message});
+    }
+});
+
+module.exports = router;
